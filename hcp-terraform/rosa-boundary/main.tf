9bc03ce62e321cc78f0de880e046044d9217c369
Shawn Bai <sbai@redhat.com>
2026-07-24 02:23:47 +1000

    T2: ROSAENG-61426: Add ROSA Boundary HCP Terraform workspaces - PR-1 (#147)
    * Add ROSA Boundary HCP Terraform workspaces
    
    Signed-off-by: Shawn Bai <sbai@redhat.com>
    
    * Add minimal Terraform roots for workload workspaces
    
    Signed-off-by: Shawn Bai <sbai@redhat.com>
    
    ---------
    
    Signed-off-by: Shawn Bai <sbai@redhat.com>

--- hcp-terraform/rosa-boundary/main.tf
+++ hcp-terraform/rosa-boundary/main.tf
@@ -1,3 +1,39 @@
 terraform {
   required_version = "1.15.8"
+
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "0.79.0"
+    }
+  }
+}
+
+module "rosa_boundary" {
+  source  = "app.terraform.io/hp-platform-engineering/workspaces/tfe"
+  version = "0.0.14"
+
+  organization      = "hp-platform-engineering"
+  project_name      = "rosa-boundary"
+  meta_project_name = "meta-rosa"
+  notification_url  = var.notification_url
+
+  workspaces = {
+    rosa-boundary-stage-network = {
+      terraform_version = "1.15.8"
+      working_directory = "deploy/network"
+      github_repo_org   = "openshift-online"
+      github_repo_name  = "rosa-boundary"
+      variables         = []
+    }
+
+    rosa-boundary-stage-aws-creds = {
+      terraform_version = "1.15.8"
+      working_directory = "hcp-terraform/aws-creds/rosa-boundary-stage"
+      github_repo_org   = "openshift-online"
+      github_repo_name  = "rosa-boundary"
+      execution_mode    = "local"
+      variables         = []
+    }
+  }
 }

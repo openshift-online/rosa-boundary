48e8e5d1322c1f417b8675ca380eca82f27ed5e3
Shawn Bai <sbai@redhat.com>
2026-08-01 00:59:55 +1000

    Add ROSA Boundary staging network stack (#173)

--- deploy/network/main.tf
+++ deploy/network/main.tf
@@ -1,3 +1,30 @@
 terraform {
   required_version = "1.15.8"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region              = var.aws_region
+  allowed_account_ids = [var.aws_account_id]
+}
+
+data "aws_region" "current" {}
+
+locals {
+  name_prefix = "${var.project}-${var.stage}"
+
+  nat_count = var.nat_strategy == "one_per_az" ? length(var.availability_zones) : 1
+
+  common_tags = merge(var.additional_tags, {
+    Project   = var.project
+    Stage     = var.stage
+    Region    = data.aws_region.current.region
+    ManagedBy = "Terraform"
+  })
 }

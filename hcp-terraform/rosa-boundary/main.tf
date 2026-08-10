3f828720d75c42f531cbcbb7760a7c950525b315
Shawn Bai <sbai@redhat.com>
2026-08-10 18:22:19 +1000

    ROSAENG-61427: configure regional staging workspace
    Signed-off-by: Shawn Bai <sbai@redhat.com>

--- hcp-terraform/rosa-boundary/main.tf
+++ hcp-terraform/rosa-boundary/main.tf
@@ -43,5 +43,70 @@ module "rosa_boundary" {
       ]
       variables = []
     }
+
+    rosa-boundary-stage-regional = {
+      terraform_version      = "1.15.8"
+      working_directory      = "deploy/regional"
+      github_repo_org        = "openshift-online"
+      github_repo_name       = "rosa-boundary"
+      auto_apply             = false
+      auto_apply_run_trigger = false
+      variable_set_names     = ["rosa-boundary-rosa-boundary-stage-default-aws-dynamic-creds"]
+      variables = [
+        {
+          key      = "aws_account_id"
+          value    = "150100906299"
+          category = "terraform"
+        },
+        {
+          key      = "aws_region"
+          value    = "us-east-1"
+          category = "terraform"
+        },
+        {
+          key      = "stage"
+          value    = "stage"
+          category = "terraform"
+        },
+        {
+          key      = "vpc_id"
+          value    = "vpc-008ef33919b443f10"
+          category = "terraform"
+        },
+        {
+          key      = "subnet_ids"
+          value    = "[\"subnet-0042826174855e520\", \"subnet-0f9392e3159168d6f\"]"
+          category = "terraform"
+          hcl      = true
+        },
+        {
+          key      = "container_image"
+          value    = "quay.io/redhat-user-workloads/rosa-tenant/rosa-boundary:c79a45da4e603c0b36576122cdadd3f2087a11fc"
+          category = "terraform"
+        },
+        {
+          key      = "keycloak_issuer_url"
+          value    = "https://auth.redhat.com/auth/realms/EmployeeIDP"
+          category = "terraform"
+        },
+        {
+          key       = "keycloak_thumbprint"
+          value     = "6541cf7958127d300fe86fd7ff324e836c75b53b"
+          category  = "terraform"
+          sensitive = true
+        },
+        {
+          key      = "required_groups"
+          value    = "[\"ai-sd-sre\"]"
+          category = "terraform"
+          hcl      = true
+        },
+        {
+          key      = "abac_tag_key"
+          value    = "uuid"
+          category = "terraform"
+        },
+      ]
+    }
   }
 }
